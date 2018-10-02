34bad46d4b1ee4b529246a7a08a62297c90ce82b
savelii.sychov <savelii.sychov@myheritage.com>
2018-10-02 18:33:53 +0300

    init

---
+++ lections/lection_1_Intro/materials/main.js
@@ -0,0 +1,80 @@
+console.log("Lection 1");
+
+
+x = 10;
+
+// console.log(typeof x, typeof 20, typeof 'Hello world');
+
+b = x + 20;
+
+// jkbas = 2;
+// str1 = 'hello';
+// str2 = "fooBar";
+
+// console.log(b, str1, "SKJASDNLK", 10);
+
+// -----------------------------------------
+
+// a = 10;
+
+// b = -20;
+
+// res = a + b;
+// console.log(res);
+
+// res = a - b;
+// console.log(a - b);
+
+// res = a * b;
+// console.log(res);
+
+// res = a / b;
+// console.log(res);
+
+// -------------------------------------------
+
+
+x = 30; // присвоить значение
+y = '345';
+
+// приведение типов
+// console.log('x - y =', x - y );
+// console.log('x * y =', x * y );
+// console.log('x / y =', x / y );
+
+// // 1 parseInt()
+// newY = parseInt(y);
+
+// // 2 унарные операторы
+
+// // x + (-1)*(x + y) => x + (-x - y) => x - x - y
+// z = '-20';
+// // +'20' => (+1)*'20' => (+1)*20 => 20
+// console.log( +z );
+
+
+// console.log('x + y =', x + +y );
+
+
+
+// --------------------
+
+// alert(newY);
+// res = +prompt('Input X', '100');
+// res2 = +prompt()
+// console.log(res + res2)
+
+
+// ----------------- %
+
+// boolean => 1, 0 // true, false
+
+console.log( 10 > 5 );
+console.log( 10 < 5 );
+console.log( 10 >= 5 );
+console.log( 10 <= 5 );
+console.log( 10 == 5 );
+console.log( 10 != 5 );
+
+
+
