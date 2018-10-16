2c4b4493d2f50041627a45fd256f8c845d84a796
savelii.sychov <savelii.sychov@myheritage.com>
2018-10-16 19:50:56 +0300

    L_4

---
+++ lections/lection_4_arrays/materials/main.js
@@ -0,0 +1,47 @@
+console.log('Lection_4');
+
+// A = [];
+
+// B = [1, 7, 8, 23, 8, 5];
+
+// console.log(B, B.length);
+
+// for(i = 0; i < B.length; i++){
+//     console.log( B[i] );
+
+//     if (B[i] % 2 == 0){
+//         B[i] = B[i]*B[i];
+//     }
+// }
+
+// console.log(B);
+
+// ---------------------------------
+
+// arrays in js
+
+
+arr = [7, 6,8 ,4,89 ,5 ,4,7 ,75 ,76];
+
+console.log(arr)
+// arr.length = 100;
+// console.log(arr)
+// arr.length = 5;
+// console.log(arr)
+// arr.length = 10;
+// console.log(arr)
+
+// for(i = 0; i < arr.length; i++){
+//     console.log( arr[i] );
+// }
+
+
+arr[20] = 'INNER';
+
+console.log(arr)
+
+// ------------------------------
+
+// A = new Array(30);
+A = new Array(30, 10);
+console.log(A)
