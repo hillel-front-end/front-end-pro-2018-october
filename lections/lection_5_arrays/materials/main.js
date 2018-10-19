7f11e1b14ff7c503a2dcb505956fc4befd3e7b50
lin4pin <skorkh.edu@gmail.com>
2018-10-19 21:07:20 +0300

    L_5_upd

--- lections/lection_5_arrays/materials/main.js
+++ lections/lection_5_arrays/materials/main.js
@@ -31,30 +31,94 @@ console.log('Lection_5');
 // -------------------------------------
 
 // create array
-n = 10;
-m = 20;
-A = new Array(n)
+// n = 10;
+// m = 20;
+// A = new Array(n)
 
-for(i = 0; i < A.length; i++){
-    A[i] = new Array(m);
-}
+// for(i = 0; i < A.length; i++){
+//     A[i] = new Array(m);
+// }
 
 // console.log(A)
 
 // fill array
 
-for(i = 0; i < A.length; i++){
-    for(j = 0; j < A[i].length; j++){
-        A[i][j] = Math.floor(Math.random()*10);
-    }
-}
+// for(i = 0; i < A.length; i++){
+//     for(j = 0; j < A[i].length; j++){
+//         A[i][j] = Math.floor(Math.random()*10);
+//     }
+// }
 
 // output array
 
 
+// for(i = 0; i < A.length; i++){
+//     for(j = 0; j < A[i].length; j++){
+//         document.write(A[i][j] + ' ');
+//     }
+//     document.write('<br />');
+// }
+
+
+// -------------------------------------
+
+n = 3;
+m = 3;
+A = new Array(n)
+
 for(i = 0; i < A.length; i++){
+    A[i] = new Array(m);
+
     for(j = 0; j < A[i].length; j++){
+        A[i][j] = Math.floor(Math.random()*5 + 1);
         document.write(A[i][j] + ' ');
     }
     document.write('<br />');
+}
+
+// min = A[0][0];
+// imin = 0;
+// jmin = 0;
+// for(i = 0; i < A.length; i++){
+//     for(j = 0; j < A[i].length; j++){
+//         if (A[i][j] < min) {
+//             min = A[i][j];
+//             imin = i;
+//             jmin = j;
+//         }
+//     }
+// }
+
+// console.log(min);
+
+// buffer = A[imin][jmin];
+// A[imin][jmin] = A[0][0];
+// A[0][0] = buffer;
+
+// document.write('<hr />');
+// for(i = 0; i < A.length; i++){
+//     for(j = 0; j < A[i].length; j++){
+//         document.write(A[i][j] + ' ');
+//     }
+//     document.write('<br />');
+// }
+
+
+sum = 0;
+for(i = 0; i < A.length; i++){
+    for(j = 0; j < A[i].length; j++){
+        sum += A[i][j];
+    }
+}
+
+av = sum / (n*m);
+
+console.log('av', av);
+console.log(A)
+
+for(i = 0; i < A.length; i++){
+    for(j = 0; j < A[i].length; j++){
+       item = (A[i][j] - av)*100/A[i][j];
+       console.log(item+'%');
+    }
 }
