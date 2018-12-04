434b6e48096928d959bff570d9c67296fec4d430
SychovSaveliy <sychov.saveliy@gmail.com>
2018-12-04 21:11:43 +0200

    L_13_upd

--- lections/lection_13_BOM_DOM/materials/main.js
+++ lections/lection_13_BOM_DOM/materials/main.js
@@ -15,51 +15,73 @@
 
 
 window.onload = function(){
-    var elem = document.getElementById('block');
+    // var elem = document.getElementById('block');
 
-    // console.dir(elem);
+    // // console.dir(elem);
 
-    // console.log(elem.id);
-
-
-    var elems = document.getElementsByClassName('example');
-    // console.log(elems);
-
-    for(var i = 0; i < elems.length; i++){
-        // console.log(elems[i].id);
-    }
-
-    // --------------------------------
-    // selector
-
-    blockItem = document.querySelector('#block');
-    var blocks = document.querySelectorAll('.block');
+    // // console.log(elem.id);
 
 
-    console.dir(blockItem);
+    // var elems = document.getElementsByClassName('example');
+    // // console.log(elems);
 
-    console.log(blockItem.innerHTML)
-    // setTimeout(function(){
-    //     blockItem.innerHTML += '!';
-    // },2000)
+    // for(var i = 0; i < elems.length; i++){
+    //     // console.log(elems[i].id);
+    // }
 
-    blockItem.innerHTML += '!';
-    for(var i = 0; i < blocks.length; i++){
-        blocks[i].innerHTML += '!'
-    }
+    // // --------------------------------
+    // // selector
 
-    // --------------------------------
-    // class
+    // blockItem = document.querySelector('#block');
+    // var blocks = document.querySelectorAll('.block');
+
+
+    // console.dir(blockItem);
+
+    // console.log(blockItem.innerHTML)
+    // // setTimeout(function(){
+    // //     blockItem.innerHTML += '!';
+    // // },2000)
+
+    // blockItem.innerHTML += '!';
+    // for(var i = 0; i < blocks.length; i++){
+    //     blocks[i].innerHTML += '!'
+    // }
+
+    // // --------------------------------
+    // // class
     
-    // blockItem.className += ' ss foo bar';
-    blockItem.classList.add('ss', 'foo');
+    // // blockItem.className += ' ss foo bar';
+    // blockItem.classList.add('ss', 'foo');
 
-    blockItem.classList.remove('foo');
-    // blockItem.classList.toggle('hide');
-    blockItem.style.backgroundColor = "#abecbe";
+    // blockItem.classList.remove('foo');
+    // // blockItem.classList.toggle('hide');
+    // blockItem.style.backgroundColor = "#abecbe";
 
-    blockItem.style.left = 300+"px";
-    blockItem.style.top = "200px";
+    // blockItem.style.left = 300+"px";
+    // blockItem.style.top = "200px";
+
+
+    var elems = document.querySelectorAll('.block-item');
+
+    setInterval(function(){
+        for(var i = 0; i < elems.length; i++){
+            elems[i].style.width = getR(0, 300) + 'px';
+            elems[i].style.height = getR(0, 300) + 'px';
+            
+            var dHeight = window.innerHeight - elems[i].clientHeight;
+            var dWidth = window.innerWidth - elems[i].clientWidth;
+            elems[i].style.left = getR(0, dWidth) + 'px';
+            elems[i].style.top = getR(0, dHeight) + 'px';
+            elems[i].style.backgroundColor = 'rgb(' + getR(0, 256)+', '+getR(0, 256)+', '+getR(0, 256)+')';
+        }
+    }, 500);
+
+
+}
+
+function getR(min, max){
+    return Math.floor(Math.random()*(max - min) + min);
 }
 
 
