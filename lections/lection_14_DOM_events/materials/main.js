013ab863dad9be534f3e114a857f12a2d9da420a
savelii.sychov <savelii.sychov@myheritage.com>
2018-12-07 20:55:58 +0200

    L_14 upd

--- lections/lection_14_DOM_events/materials/main.js
+++ lections/lection_14_DOM_events/materials/main.js
@@ -44,8 +44,8 @@ window.onload = function(){
     // item.onmouseover = handler;
     // item.onmouseout = handler;
     // item.onmousemove = handler;
-    item.onmousedown = handler;
-    item.onmouseup = handler;
+    // item.onmousedown = handler;
+    // item.onmouseup = handler;
 
     // --------------------------------------
 
@@ -67,4 +67,34 @@ window.onload = function(){
     //     console.log('handler 2')
     // });
 
+
+
+
+var flag = false;
+    item.addEventListener('mousedown', function(ev){
+        flag = true;
+        var startX = ev.clientX;
+        var startY = ev.clientY;
+
+        var itemX = this.offsetLeft;
+        var itemY = this.offsetTop;
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        function onMouseMove(event){
+            if (!flag) return;
+            var curX = event.clientX;
+            var curY = event.clientY;
+
+
+            item.style.left = itemX + (curX - startX) + 'px';
+            item.style.top = itemY + (curY - startY) + 'px';
+        }
+
+
+        ev.target.addEventListener('mouseup', function(){
+            flag = false;
+            document.removeEventListener('mousemove', onMouseMove)
+        })
+    })
 }
