543b3ea0438addb43f4434a02c12b99654a41ade
SychovSaveliy <sychov.saveliy@gmail.com>
2018-12-18 21:08:17 +0200

    L_17 upd

--- lections/lection_17_Storage-practice/materials/main.js
+++ lections/lection_17_Storage-practice/materials/main.js
@@ -5,18 +5,18 @@ console.log('Lection_17');
 
 // local Storage
 
-console.log(localStorage);
+// console.log(localStorage);
 
 
 
-if (localStorage.x) {
-    document.write(localStorage.x);
-} else {
-    localStorage.x = prompt();
-}
+// if (localStorage.x) {
+//     document.write(localStorage.x);
+// } else {
+//     localStorage.x = prompt();
+// }
 
-localStorage.DDID = 'fooName'
-localStorage.setItem('DDID', 'fooName');
+// localStorage.DDID = 'fooName'
+// localStorage.setItem('DDID', 'fooName');
 // delete localStorage.x; 
 // localStorage.removeItem('x');;
 
@@ -40,8 +40,8 @@ var list = `
     [1, 10, false, [2, 8]]
 `;
 
-var parseData = JSON.parse(data);
-console.log(data, parseData);
+// var parseData = JSON.parse(data);
+// console.log(data, parseData);
 
 
 var object = {
@@ -51,5 +51,30 @@ var object = {
     list: [1, 8, 9]
 }
 
-var jsonData = JSON.stringify(object);
-console.log(jsonData)
+// var jsonData = JSON.stringify(object);
+// console.log(jsonData);
+
+
+
+window.onload = function(){
+    var blocks = document.querySelectorAll('.block');
+
+
+    for(var i = 0; i < blocks.length; i++){
+        var block = blocks[i];
+        var span = block.querySelector('span');
+        var button = block.querySelector('.click');
+        button.dataset.id = 'block-'+i;
+
+        var counter = localStorage['block-'+i] || 0;
+        span.innerHTML = counter;
+
+        button.addEventListener('click', onClick)
+    }
+}
+
+function onClick(){
+    var counter = localStorage[this.dataset.id] || 0;
+    this.previousElementSibling.innerHTML = ++counter;
+    localStorage[this.dataset.id]  = counter;
+}
