d46389f32af44a2e0a8fd4a76478f2b29eaeff59
savelii.sychov <savelii.sychov@myheritage.com>
2019-01-29 19:47:52 +0200

    L_24

---
+++ lections/lection_24_vuejs_single-file-component/materials/fr-pro-2018/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    
+    <img alt="Vue logo" src="./assets/logo.png">
+    {{ text }}
+
+    <ul>
+      <li v-for="(value, index) in list" :key="index">
+        {{ value }}
+      </li>
+    </ul>
+
+    <Flag></Flag>
+  </div>
+</template>
+
+<script>
+import Flag from './components/Flag';
+
+export default {
+  name: 'app',
+  components: {
+    Flag
+  },
+  data(){
+    return {
+      text: 'Hello world!',
+      list: [
+        2, 2, 5, 9, 7
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+div {
+
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lections/lection_24_vuejs_single-file-component/materials/fr-pro-2018/src/components/Flag.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flag" @click="onClick">Flag!</div>
+</template>
+
+<script>
+export default {
+    name: "Flag",
+    methods: {
+        onClick(){
+            console.log('click');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
